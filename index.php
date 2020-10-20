61a84e0f8fa23288aeecd9ec0378143c6a7edb69
mole <georgegrammozis@otenet.gr>
2020-10-20 16:18:31 +0300

    setting up strucure

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
+<?php 
+	require("config.php");
+	require("php/functions.php");
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
 	
-	<meta name="keywords" content="">
-	
-	<meta name="description" content="">
+	<meta name="keywords" content="<?php echo $meta_page_keywords["index-page"]; ?>">
+	<meta name="description" content="<?php echo $meta_page_description["index-page"]; ?>">
 	
 	<meta name="language" content="el-gr" />
-	<!-- <meta name="Robots" content="index, follow" /> -->
+	<meta name="Robots" content="index, follow" />
 	<meta name="googlebot" content="index, follow" />
 	
 	<!-- <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png"> -->
@@ -17,19 +20,20 @@
 	<!-- <link rel="manifest" href="/site.webmanifest"> -->
 
 	<!-- Structured data in json fornat -->
-	<?php include('html/structured-data-json.php'); ?>
+	<?php //include('html/structured-data-json.php'); ?>
 	
 	<!-- Google analytics script go uncomment when publishing !IMPORTANT-->
 	<?php //include('html/google-analytics.php'); ?>
 
-	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/font-awesome/css/all.min.css" />
-	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,500&display=swap&subset=greek-ext" rel="stylesheet">
-	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/main.css" />
+	<link rel="stylesheet" type="text/css" href="css/media-queries.css" />
+	<title><?php echo $brand_name; ?></title>
 </head>
 <body>
-	<?php include 'includes/header.php'; ?>
 	<?php include 'includes/nav.php'; ?>
+	<?php include 'includes/slides.php'; ?>
 
 
 	
